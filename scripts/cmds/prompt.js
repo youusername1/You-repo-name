dff8d692c89ff889ca51bf64423f503e59291b61
JulioRaven <114471308+JulioRaven@users.noreply.github.com>
2025-05-17 21:45:47 +0100

    Update prompt.js

--- scripts/cmds/prompt.js
+++ scripts/cmds/prompt.js
@@ -6,7 +6,7 @@ module.exports = {
     name: "prompt",
     aliases: ["p"],
     version: "1.2",
-    author: "Team Calyx",
+    author: "MarianCross",
     countDown: 5,
     role: 0,
     longDescription: {
